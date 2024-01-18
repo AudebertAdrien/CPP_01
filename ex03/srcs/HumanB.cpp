a5e84f26ecd86e1fe2a9d96d7c0d12ad6c968f8a
Adrien Audebert <aaudeber@student.42perpignan.fr>
2024-01-18 17:07:57 +0100

    fix issue

--- ex03/srcs/HumanB.cpp
+++ ex03/srcs/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:46:49 by motoko            #+#    #+#             */
-/*   Updated: 2024/01/16 19:35:13 by motoko           ###   ########.fr       */
+/*   Updated: 2024/01/18 17:07:19 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 #include "HumanB.hpp"
 
 HumanB::HumanB() : _name(), _weapon() {
-	std::cout << "HumanA Constructor" << std::endl;
+	std::cout << "HumanB Constructor" << std::endl;
 }
 
 HumanB::HumanB(std::string name) : _name(name), _weapon() {
-	std::cout << "HumanA Constructor with params" << std::endl;
+	std::cout << "HumanB Constructor with params" << std::endl;
 }
 
 HumanB::~HumanB() {
