ac359b3ba47fb56fd5af50e714f2b661929b3983
Adrien Audebert <aaudeber@student.42perpignan.fr>
2024-01-19 16:52:14 +0100

    fix ex05

--- ex05/srcs/Harl.cpp
+++ ex05/srcs/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 17:03:45 by motoko            #+#    #+#             */
-/*   Updated: 2024/01/18 14:46:50 by motoko           ###   ########.fr       */
+/*   Updated: 2024/01/19 16:48:05 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,13 @@ Harl::Harl() {
 
 void	Harl::complain(std::string level)
 {
-	if (level == "DEBUG")
-		(this->*_t[0])();
-	if (level == "INFO")
-		(this->*_t[1])();
-	if (level == "WARNING")
-		(this->*_t[2])();
-	if (level == "ERROR")
-		(this->*_t[3])();
+	const char* levelString[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levelString[i] == level)
+			(this->*_t[i])();
+	}
 }
 
 void	Harl::debug(void)
